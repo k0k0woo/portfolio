5073b5f44699b2ddc2cc3e2ce15aab73a258ebe9
Sky-Josh <josh@skyliftuav.co.uk>
2025-01-31 16:29:09 +0000

    feat: individual stock page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TickerBar from "./components/TickerBar"; 
+//import StockHeatmap from './components/StockHeatmap';
+import StockInfo from './components/StockInfo';
+import Financials from './components/Financials';
+import Technicals from './components/Technicals';
+import Chart from './components/Chart';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Don't Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* ticker bar */}
+      <TickerBar />
+      {/* heatmap */}
+      {/*<StockHeatmap/>*/}
+
+      {/*stock info */}
+      <div className='flex-row'>
+        <div className='flex-column'>
+        <StockInfo/>
+        <Financials/>
+        </div>        
+        
+        <Chart/>
+        
+        <Technicals/>
+
+      </div>
+
+
     </div>
   );
 }
