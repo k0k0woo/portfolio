62c1306ecba6d15e515a8fd5a0648da7d0d50457
Ubuntu <ubuntu@ip-172-31-17-110.eu-west-2.compute.internal>
2025-02-26 12:58:50 +0000

    port-website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage"; // Your main page
-import ChartPage from "./pages/ChartPage"; // Your new Chart page
-import ValueCalculator from "./pages/ValueCalculator"; // Your new Chart page
+import ChartPage from "./pages/stocks/ChartPage"; // Your new Chart page
+import ValueCalculator from "./pages/stocks/ValueCalculator"; // Your new Chart page
 import NotFound from "./pages/NotFound"; // Optional: Handles unknown routes
+import StockHomePage from './pages/stocks/stockHome';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chart" element={<ChartPage />} />
-        <Route path="/value" element={<ValueCalculator />} />
+        <Route path="/stock/chart" element={<ChartPage />} />
+        <Route path="/stock/value" element={<ValueCalculator />} />
+        <Route path="/stock" element={<StockHomePage />} />
         <Route path="*" element={<NotFound />} /> {/* 404 page */}
       </Routes>
     </Router>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,26 +3,11 @@ import { Link } from "react-router-dom";
 
 
 const HomePage = () => {
-  useEffect(() => {
-    const fetchStockData = async () => {
-      try {
-        const response = await fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.REACT_APP_FINNHUBIO_API_KEY}`);
-        const data = await response.json();
-        //setStocks(data);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching stock data:", error);
-      }
-    };
-
-    fetchStockData();
-  }, []);
   return (
     <div className="page">
       <h1>Home Page</h1>
-      <p>Welcome to the stock tracking app.</p>
-      <p><Link to="/chart">Go to Chart Page</Link></p>
-      <p><Link to="/Value">Go to Value Page</Link></p>
+      <p>Welcome to my app.</p>
+      <p><Link to="/Stock">Go to Stock Page</Link></p>
     </div>
   );
 };
